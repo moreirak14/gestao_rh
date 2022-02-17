c4768b37605d0b2639c8cfe9ab6015caa7078db8
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-02-17 19:02:37 -0300

    CRUD employee

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import (
+    FuncionariosList,
+    FuncionarioEdit,
+    FuncionarioDelete,
+    FuncionarioCreate,
+)
 
 
 urlpatterns = [
-    path("", home),
+    path("", FuncionariosList.as_view(), name="list_funcionarios"),
+    path("editar/<int:pk>", FuncionarioEdit.as_view(), name="update_funcionario"),
+    path("delete/<int:pk>", FuncionarioDelete.as_view(), name="delete_funcionario"),
+    path("novo/", FuncionarioCreate.as_view(), name="create_funcionario"),
 ]
