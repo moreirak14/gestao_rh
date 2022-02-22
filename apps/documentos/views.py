d26cbaffcde9b4080f95eb82f7b8bfb21b619dde
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-02-21 21:43:08 -0300

    CRUD documents

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Documento
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class DocumentoCreate(CreateView):
+    model = Documento
+    fields = ["descricao", "arquivo"]
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.pertence_id = self.kwargs['funcionario_id']
+
+        return self.form_valid(form) if form.is_valid() else self.form_invalid(form)
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +85,7 @@ WSGI_APPLICATION = "gestao_rh.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -123,8 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
