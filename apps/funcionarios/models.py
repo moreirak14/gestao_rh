c9485d1671d13818578cd6a8a32c54bdf89881dc
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-03-02 13:46:25 -0300

    Added new features in hours

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.db.models import Sum
 
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
@@ -16,6 +17,10 @@ class Funcionario(models.Model):
 
     def get_absolute_url(self):
         return reverse("list_funcionarios")
+
+    @property
+    def total_horas_extra(self):
+        return self.registrohoraextra_set.all().aggregate(Sum("horas"))["horas__sum"]
 
     def __str__(self):
         return self.nome
